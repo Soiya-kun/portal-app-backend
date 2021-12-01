997ba88ce5261bddd7ef25fd8338c6547929b52f
seiwa-ic-system <s-system@seiwa-ic.co.jp>
2021-12-01 16:45:41 +0900

    リクエストボディ

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from enum import Enum
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
@@ -10,28 +12,41 @@ class ModelName(str, Enum):
     z = "z"
 
 
+class RestInfo(BaseModel):
+    name: str
+    date: int
+    morning: bool = False
+    afternool: bool = False
+    reason: Optional[str] = None
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/user")
+@app.post("/")
+async def testpost():
+    return {"testpost": "testpost"}
+
+
+@app.get("/testuser")
 async def getUser():
     return {"user_id": "testuser"}
 
 
 @app.get("/{user_id}")
-async def getAnotherUser(user_id: str):
+async def getAnotherUser(user_id: str, a: int = 0, b: int = 0):
     if user_id == ModelName.x:
-        return {"user_id": "x_man"}
+        return {"user_id": "x_man", "result": a + b}
     elif user_id == ModelName.y:
-        return {"user_id": "y_man"}
+        return {"user_id": "y_man", "result": a + b}
     elif user_id == ModelName.z:
-        return {"user_id": "z_man"}
+        return {"user_id": "z_man", "result": a + b}
     else:
-        return {"user_id": user_id}
+        return {"user_id": user_id, "result": a + b}
 
 
-@app.post("/")
-async def testpost():
-    return {"testpost": "testpost"}
+@app.post("/{user_id}")
+async def postRest(restInfo: RestInfo):
+    return restInfo
