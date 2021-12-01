30ea342b506d7539b2bb8ae3c62b4408949d7150
seiwa-ic-system <s-system@seiwa-ic.co.jp>
2021-12-01 13:30:43 +0900

    パスパラメータ

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,37 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
+
+
+class ModelName(str, Enum):
+    x = "x"
+    y = "y"
+    z = "z"
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/user")
+async def getUser():
+    return {"user_id": "testuser"}
+
+
+@app.get("/{user_id}")
+async def getAnotherUser(user_id: str):
+    if user_id == ModelName.x:
+        return {"user_id": "x_man"}
+    elif user_id == ModelName.y:
+        return {"user_id": "y_man"}
+    elif user_id == ModelName.z:
+        return {"user_id": "z_man"}
+    else:
+        return {"user_id": user_id}
+
+
+@app.post("/")
+async def testpost():
+    return {"testpost": "testpost"}
